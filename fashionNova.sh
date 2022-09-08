92442fbc5394346062837148d03dcb3921ba7037
queenoftech <natinda2006@gmail.com>
2022-09-08 19:48:36 +0000

    first commit

---
+++ fashionNova.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#This scripts will finalize the negotiation for purchasing shares in fashionNova
+#please review,modify and execute script accordingly
+echo "please enter name"
+read name
+echo "please enter offer amount"
+read fee
+if [ $fee -ge 10000 ]
+then
+echo "congratulations $name your offer amount has been accepted"
+echo "welcome to the fashioNova family"
+echo "you are now a fashioNova share holder"
+else
+echo "sorry $name offer denied"
+fi
+whoami
+date
